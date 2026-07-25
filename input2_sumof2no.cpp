f98d202cd04e06210365a8f712c8dd28f5ef648c
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-07-25 16:30:10 +0530

    cpp

---
+++ input2_sumof2no.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"enter the a:";
+	cin>>a;
+	cout<<"enter the b:";
+	cin>>b;
+	int sum=a+b;
+	cout<<"sum:"<<sum;
+	return 0;
+}

14d1f1ec96a604e4a4f940f3c0a5ccb93ea6d77d
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-07-25 19:00:14 +0530

    cpp

---
+++ ifelse_threedigitno.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using  namespace std;
+int main(){
+	cout<<"enter the no:";
+	int n;
+	cin>>n;
+	if(n>=100 && n<=999){
+		cout<<"three digit no";
+	}
+	else{
+		cout<<"not a three digit no";
+	}
+}
+

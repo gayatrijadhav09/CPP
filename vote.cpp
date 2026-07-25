96f56396f6f129c98f4e4725fb1084049772bc4c
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-07-25 18:34:30 +0530

    cpp

---
+++ vote.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int age;
+	cout<<"enter the age:"<<endl;
+	cin>>age;
+	if(age>=18){
+		cout<<"you can vote\n";
+	}
+	else{
+		cout<<"you cant vote\n";
+	}
+	return 0;
+}
+	

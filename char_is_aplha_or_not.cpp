55781c6c7cb4c392739266d9f900a4c6ab53823c
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-07-25 20:42:35 +0530

    cpp

---
+++ char_is_aplha_or_not.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout<<"enter the character:";
+	cin>>ch;
+	int ascii=(int)ch;
+	if(ascii>=97 && ascii<=122){
+		cout<<"the character is lowercase alphabet";
+	}
+	if(ascii>=65 && ascii<=90){
+		cout<<"the character is uppercase alphabet";
+	}
+	return 0;
+}

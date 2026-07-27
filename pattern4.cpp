3fd4584d77e2c115d0c7ffe379aef82dbc352cd8
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-07-27 20:06:14 +0530

    cpp

---
+++ pattern4.cpp
@@ -0,0 +1,20 @@
+/*ABCD
+  ABCD
+  ABCD
+  ABCD */
+
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=1;i<=4;i++){
+		char ch='A';
+		for(int i=1;i<=4;i++){
+			 cout<<ch;
+
+			ch=ch+1;
+			
+		}
+		cout<<endl;
+	}
+	return 0;
+}

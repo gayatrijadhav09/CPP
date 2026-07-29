f2a12b7f53453f795158df61c3d63ba74e9fa59a
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-07-29 15:47:09 +0530

    cpp

---
+++ pattern8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch='A';
+	
+	for(int i=1;i<4;i++){
+		for(int j=1;j<4;j++){
+			cout<<ch;
+			ch++;
+		}
+		cout<<endl;
+	}
+	return 0;
+}

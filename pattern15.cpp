e834e500503a6c1879f3ec08b8e6026848a32187
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-07-29 22:00:21 +0530

    cpp

---
+++ pattern15.cpp
@@ -0,0 +1,24 @@
+
+/* A
+   BB
+   CCC
+   DDDD */
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	
+	char ch='A';
+	for(int i=1;i<=4;i++){
+		for(int j=1;j<=i;j++){	
+			cout<<ch;
+	
+		}
+
+	
+		cout<<endl;
+	ch++;
+	}
+	return 0;
+}

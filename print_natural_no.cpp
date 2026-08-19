dd5d35db06368ae59a2d55298e9ee1c5d6d95a29
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-08-19 09:47:27 +0530

    cpp

---
+++ print_natural_no.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int natural(int n){
+	for(int i=1;i<n;i++){
+	cout<<i<<endl;
+	}
+    return n;
+}
+
+int main(){
+	cout<<natural(30)<<endl;
+	return 0;
+}
+    

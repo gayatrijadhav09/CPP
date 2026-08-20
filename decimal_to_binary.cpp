8680287f9b9f86a32e56e65249f05adec4a21ffe
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-08-20 08:08:27 +0530

    cpp

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int dectobinary(int decno){
+	 int ans=0;
+	 int pow=1;
+	 while(decno>0){
+		int  rem=decno%2;
+		 decno=decno/2;
+		 ans+=(rem*pow);
+		 pow=pow*10;
+	 }
+	 return ans;
+ }
+int main(){
+	int decno=10;
+	cout<<dectobinary(decno)<<endl;
+	return 0;
+}
+

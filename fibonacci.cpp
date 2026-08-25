2f2aa0a6d3657f2c32e341e7d5cefa82980feaa5
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-08-25 07:50:23 +0530

    cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int fib(int n);
+int main(){
+	cout<<fib(6);
+	return 0;
+}
+int fib(int n){
+	if(n==0){
+		return 0;
+	}
+		if(n==1){
+			return 1;
+		}
+	int fibnm1=fib(n-1);
+	int fibnm2=fib(n-2);
+	int fibn=fibnm1+fibnm2;
+//cout<<"fib:"<<n<<" "<<fibn<<endl;
+	return fibn;
+}

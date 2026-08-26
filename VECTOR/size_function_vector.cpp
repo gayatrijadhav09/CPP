4eddacdb694d8366292c56e554cfd4ba5f49ea80
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-08-26 16:52:45 +0530

    vector

---
+++ VECTOR/size_function_vector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+               vector<char>letter= {'a','b','c','d'};
+               cout<<"size="<<letter.size()<<endl;
+
+        for(char i : letter){
+        cout<<i<<endl;
+        }
+        return 0;
+}
+                                                                                                                                                     
+

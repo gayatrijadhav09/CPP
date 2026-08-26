6b2c6cd7353f352560849484b406961981b88db8
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-08-26 16:52:16 +0530

    vector

---
+++ VECTOR/loop1_vector.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	       vector<char>letter= {'a','b','c','d'};
+	       
+
+	for(char i : letter){
+	cout<<i<<endl;
+	}
+	return 0;
+}
+

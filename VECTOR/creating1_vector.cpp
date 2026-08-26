632fc95c2bb07f8daf1f1c55629f6c31cbd8ea61
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-08-26 16:51:27 +0530

    vector

---
+++ VECTOR/creating1_vector.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	vector<int>num(3,1);//size of vector,index of vectorvalues
+	cout<<num[0]<<endl;       
+       	cout<<num[1]<<endl;
+        cout<<num[2]<<endl;
+	return 0;
+}
+
+

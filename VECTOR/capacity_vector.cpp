65dfa1715a526d0e5adf47ea718768fa9c4ea6cc
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-08-28 10:18:29 +0530

    vector

---
+++ VECTOR/capacity_vector.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+vector<int>num;
+cout<<"size:"<<num.size()<<endl;;
+num.push_back(1);
+num.push_back(2);
+num.push_back(3);
+num.push_back(4);
+num.push_back(5);
+cout<<"size:"<<num.size()<<endl;
+
+cout<<"capacity:"<<num.capacity()<<endl;
+
+for(int i :num){
+        cout<<i<<endl;
+}
+return 0;
+}
+

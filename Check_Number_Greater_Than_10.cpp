06ecd5c903187a89026ae4d5577a5f0dbcda65a5
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-08-29 21:40:13 +0530

    cpp

---
+++ Check_Number_Greater_Than_10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n > 10)
+        cout << "Greater than 10";
+    else
+        cout << "Less than or equal to 10";
+
+    return 0;
+}

d7686f42a13f6b889c4838bc11fb1d23cea1dad3
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-09-06 09:57:35 +0530

    cpp

---
+++ multi_ptr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int *p1, *p2;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    p1 = &a;
+    p2 = &b;
+
+    cout << "Multiplication = " << (*p1 * *p2) << endl;
+
+    return 0;
+}

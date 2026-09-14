4e2720e2c57ece4d1088587e3a9461e869c6314c
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-09-14 18:57:03 +0530

    array

---
+++ ARRAY/bubble_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void bubble_sort(int arr[],int n){
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
+ swap(arr[j],arr[j+1]);
+			}
+		}
+	}
+}
+void printarray(int arr[],int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<< " ";
+	       }
+	cout<<endl;	
+}
+int main(){
+	int n=5;
+	int arr[]={4,1,5,2,3};
+	bubble_sort(arr,n);
+	printarray(arr,n);
+	return 0;
+}

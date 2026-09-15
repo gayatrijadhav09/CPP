e382849ed3d5ba69927eab59a6fe80d5b770b9c0
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-09-15 07:22:27 +0530

    vector

---
+++ VECTOR/sort_an_array_0s_1s_2s.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+}
+
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    sortColors(nums);
+
+    for(int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}

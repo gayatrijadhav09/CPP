82c45427fa766c0b300e9dddadc2627d4f3cb68b
gayatrijadhav09 <gayatrijadhav478@gmail.com>
2026-09-23 07:57:35 +0530

    array

---
+++ ARRAY/max_consecutive_ones.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {1, 1, 0, 1, 1, 1};
+
+    int count = 0;
+    int maxCount = 0;
+
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(nums[i] == 1)
+        {
+            count++;
+        }
+        else
+        {
+            count = 0;
+        }
+
+        maxCount = max(maxCount, count);
+    }
+
+    cout << maxCount;
+
+    return 0;
+}
